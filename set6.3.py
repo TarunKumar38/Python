c652acbf532caca8423265ec5cfd5cb4b0a5b616
TarunKumar38 <51771846+TarunKumar38@users.noreply.github.com>
2019-06-25 08:15:39 +0530

    set6.3.py

--- set6.3.py
+++ set6.3.py
@@ -1,20 +1,11 @@
 num=int(input('enter a number : '))
-dig=0
 sum=0
 temp=num
 
 while temp>0 :
-	dig=dig+1
-	temp=temp//10
-
-temp=num
-i=1
-while i<=dig :
 	rem=temp%10
-	if i==1 or i==dig :
+	if temp==num or temp//10==0 :
 		sum=sum+rem
 	temp=temp//10
-	i+=1
 	
 print(sum)
-		
